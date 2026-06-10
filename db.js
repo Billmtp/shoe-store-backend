b48e461baed76fae33e8cc37417a97d56cae3b5d
Billmtp <trang0902788271@gmail.com>
2026-06-10 06:43:32 -0700

    checkvar2

--- db.js
+++ db.js
@@ -1,14 +1,10 @@
-console.log("HOST =", process.env.DB_HOST);
-console.log("PORT =", process.env.DB_PORT);
-console.log("USER =", process.env.DB_USER);
-console.log("DB =", process.env.DB_NAME);
 require("dotenv").config();
 
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -16,16 +12,17 @@ const db = mysql.createConnection({
   ssl: {
     rejectUnauthorized: false,
   },
+
+  connectTimeout: 10000,
 });
 
 db.connect((err) => {
+  if (err) {
+    console.error("Lỗi kết nối:", err);
+    return;
+  }
 
-    if (err) {
-        console.log("Lỗi kết nối:", err);
-        return;
-    }
-
-    console.log("Đã kết nối MySQL");
+  console.log("Đã kết nối MySQL");
 });
 
 module.exports = db;
